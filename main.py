f41efcffb9c83402f1b3c3654581d22be5041052
hgillis <hgillis@vdab.be>
2019-02-08 09:00:13 +0100

    swaggerify -> swagger 2 full support

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from swaggparser.parser import SwaggParser
 
 def main():
     sp = SwaggParser()
-    #sp.preparse()
-    #sp.create_pojos()
-    #sp.sufparse()
-    # sp.apify()
+    sp.preparse()
+    sp.create_pojos()
+    sp.sufparse()
+    sp.apify("petstore", "Petstore")
 
 
 if __name__ == '__main__':
--- swaggparser/parser.py
+++ swaggparser/parser.py
@@ -1,5 +1,6 @@
 import subprocess
 import datetime
+import random
 import shlex
 import json
 import uuid
@@ -24,10 +25,16 @@ class SwaggParser(object):
         if not os.path.exists(self.tmp_dir):
             os.makedirs(self.tmp_dir)
 
+        if not os.path.exists(self.root + "/html"):
+            os.makedirs(self.root + "/html")
+
+        if not os.path.exists(self.root + "/json"):
+            os.makedirs(self.root + "/json")
+
         for branch in self.branches:
             d = {}
             with open(self.root + 'html/' + branch.split('.')[0] + '.html', 'r') as f:
-                html = BeautifulSoup(f.read())
+                html = BeautifulSoup(f.read(), "lxml")
                 if self.version == 2:
                     methods = html.find_all(class_="opblock-summary-method")
                     paths = html.find_all(class_="opblock-summary-path")
@@ -36,38 +43,36 @@ class SwaggParser(object):
                     responses = [
                         [
                             {
-                                "type": "json" if response.find("example") is not None and
-                                                  response.find("example").get_text().startswith("{") else "class",
-                                "body": json.dumps(response.find("example").get_text())
-                                if response.find("example") is not None and
-                                   response.find("example").get_text().startswith("{") else None
+                                "type": "json" if response.find("pre", class_="example") is not None and
+                                                  response.find("pre", class_="example").get_text().startswith("{") else "class",
+                                "body": response.find("pre", class_="example").get_text().replace('\n', '')
+                                if response.find("pre", class_="example") is not None and
+                                   response.find("pre", class_="example").get_text().startswith("{") else None
                             }
                             for response in container.find_all("tr", class_="response") if response.get("data-code") == "200"
                         ] for container in response_containers
                     ]
-                    print([parameter for parameter in parameter_containers[5].find(class_="parameters")])
                     parameters = [
                         [
                             {
                                 "class": parameter.find(class_="parameter__in").get_text().
                                     replace('(', '').replace(')', '')
                                 if parameter.find(class_="parameter__in") is not None else None,
-                                "name": parameter.find(class_="parameter__name").get_text().split('*')[0][:-1]
+                                "name": parameter.find(class_="parameter__name").get_text().split('*')[0].split('\u00a0')[0]
                                 if parameter.find(class_="parameter__name") is not None else None
                                 ,
-                                "type": container.find(class_="body-param__example").get_text()
+                                "type": container.find(class_="body-param__example").get_text().replace('\n', '')
                                 if parameter.find(class_="parameter__in") is not None and
                                    parameter.find(class_="parameter__in").get_text().
-                                       replace('(', '').replace(')', '') == "body"
+                                       replace('(', '').replace(')', '').replace('\n', '') == "body"
                                 else parameter.find(class_="parameter__type").get_text().split('(')[0]
                                 if parameter.find(class_="parameter__type") is not None else None
-                            } for parameter in [container.find(class_="parameters")] #FIXME
+                            } for parameter in (container.find(class_="parameters").find_all('tr')[1:]
+                        if container.find(class_="parameters") is not None else [])
                         ] for container in parameter_containers
                     ]
 
-                    print(len(paths), len(methods), len(parameters), len(responses))
                     for i, path in enumerate(paths):
-                        print(path.get_text(), methods[i].get_text())
                         if path not in d:
                             d[path.get_text()] = {}
                         d[path.get_text()][methods[i].get_text()] = {
@@ -77,29 +82,36 @@ class SwaggParser(object):
                             "response": responses[i]
                         }
 
-            print(json.dumps(d, indent=4, sort_keys=True))
+            json.dump(d, open(self.root + "/json/" + branch.split('.')[0] + ".json", 'w'), indent=4, sort_keys=True)
 
-            exit(404)
-            with open(self.root + 'json/' + branch.split('.')[0] + '.json', 'r') as f:
+            with open(self.root + '/json/' + branch.split('.')[0] + '.json', 'r') as f:
                 swagger_ui = json.load(f)
 
             for key, call in swagger_ui.items():
                 for key, method in call.items():
-                    if "response" in method:
-                        if method["response"]["type"] == "json":
+                    for element in method["response"]:
+                        if element["type"] == "json":
                             with open(self.tmp_dir + "_" + str(uuid.uuid4()) + '.json', 'w') as f:
-                                json.dump(json.loads(method["response"]["value"]), f)
-                    for element in method["request"]["params"]:
-                        if element["type"] == "body":
-                            with open(self.tmp_dir + element["name"] + '.json', 'w') as f:
-                                json.dump(json.loads(element["param"]), f)
+                                json.dump(json.loads(element["body"]), f)
+                    for element in method["request"]:
+                        if element["type"].startswith("{"):
+                            if not os.path.exists(self.tmp_dir + element["name"] + '.json'):
+                                with open(self.tmp_dir + element["name"] + '.json', 'w') as f:
+                                    json.dump(json.loads(element["type"]), f)
+                            else:
+                                with open(self.tmp_dir + "_" + str(uuid.uuid4()) + '.json', 'w') as f:
+                                    json.dump(json.loads(element["type"]), f)
 
         outfolder = self.output_folder + '/' + self.base_class.replace('.', '/') + '/'
+        if not os.path.exists(outfolder):
+            os.makedirs(outfolder)
+
         for filename in os.listdir(outfolder):
             try:
+                print("remove: " + outfolder + filename)
                 os.remove(outfolder + filename)
-            except Exception:
-                pass
+            except FileNotFoundError:
+                print(outfolder + filename)
 
     def apify(self, project="vindeenjob", cls="VEJ"):
         header = """
@@ -137,46 +149,59 @@ public class %sAPI {
                     body = ""
                     _path = []
                     if "request" in method:
-                        for param in method["request"]["params"]:
-                            if param["type"] == "query":
+                        for param in method["request"]:
+                            """
+                            if param["class"] == "query":
                                 query += '"%s=" + %s + "&" + ' % (param["name"], param["name"])
                                 _path.append({
                                     "value": param["name"],
                                     "type": param["param"] if param is not "string" else "String"
                                 })
-                            elif param["type"] == "path":
+                            """
+                            if param["class"] == "path":
                                 _path.append({
                                     "value": param["name"],
-                                    "type": param["param"] if param is not "string" else "String"
+                                    "type": param["type"].capitalize()
                                 })
-                            elif param["type"] == "body":
-                                body = ".body(" + param["name"].capitalize()+ ")"
+                            elif param["class"] == "body":
+                                body = ".body(" + param["name"].capitalize() + ")"
 
                     if len(query) == 1:
                         query = ""
                     else:
                         query = query[:-9] + '"'
-                    if "response" in method:
-                        obj = "Object"
-                        if not method["response"]["type"] == "json":
-                            obj = method["response"]["type"].capitalize()
 
-                        if method["response"]["value"] is not None and method["response"]["value"].startswith('['):
-                            print(str("""
+                    obj = "Object"
+                    if "response" in method:
+                        for param in method["response"]:
+                            if not param["type"] == "json":
+                                obj = param["type"].capitalize()
+
+                            if param["type"] is "json" and param["body"].startswith('['):
+                                print(str("""
     public ArrayList<Object> getObjects(%s) {
         return rest%s.%s("%s|).statusCode(200).extractAsList(%s.class);
     }
 """ % (", ".join(p["type"] + " " + p["value"] for p in _path),
    body, req.lower(),
    path.replace('{',  '" + ').replace('}', ' + "') + query, obj)).replace('+ "|', '').replace('|', '"').replace('?"', '?').replace('""', ''))
-                        else:
-                            print(str("""
+                            else:
+                                print(str("""
     public Object getObject(%s) {
         return rest%s.%s("%s|).statusCode(200).extractAs(%s.class);
     }
 """ % (", ".join(p["type"] + " " + p["value"] for p in _path),
    body, req.lower(),
    path.replace('{',  '" + ').replace('}', ' + "') + query, obj)).replace('+ "|', '').replace('|', '"').replace('?"', '?').replace('""', ''))
+                            break
+                        else:
+                            print(str("""
+    public void voidRequest(%s) {
+        return rest%s.%s("%s|).statusCode(200);
+    }
+""" % (", ".join(p["type"] + " " + p["value"] for p in _path),
+   body, req.lower(),
+   path.replace('{',  '" + ').replace('}', ' + "') + query)).replace('+ "|', '').replace('|', '"').replace('?"', '?').replace('""', ''))
 
         print('}')
 
@@ -211,7 +236,9 @@ public class %sAPI {
                 pass
 
     def sufparse(self):
-        sleep(15)
+        for _ in range(random.randint(10, 16), 0, -1):  # Hehe random :-)
+            sleep(1)
+            print("Wacht nog %s seconden tot het verwerkt is" % _)
         t = []
         outfolder = self.output_folder + '/' + self.base_class.replace('.', '/') + '/'
         for filename in os.listdir(outfolder):
@@ -219,7 +246,6 @@ public class %sAPI {
                 t.append(outfolder + filename)
 
         for f in t:
-            print(f)
             try:
                 os.remove(f)
             except FileNotFoundError:
